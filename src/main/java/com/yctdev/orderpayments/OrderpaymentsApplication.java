a561165218eb1b32b8f53273518e1c00519eab9a
Piyush Gaur <piyushgaur0001@gmail.com>
2026-08-08 23:06:15 +0530

    Initial setup

---
+++ src/main/java/com/yctdev/orderpayments/OrderpaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.yctdev.orderpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderpaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderpaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yctdev/orderpayments/OrderpaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yctdev.orderpayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderpaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
